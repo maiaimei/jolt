4e3256d11d23519bc4ed2995d96ef5af36b2a3a8
maiaimei <1211674185@qq.com>
2024-01-05 00:07:28 +0800

    jolt test

--- jolt-samples/src/test/java/cn/maiaimei/example/ModifierTest.java
+++ jolt-samples/src/test/java/cn/maiaimei/example/ModifierTest.java
@@ -1,16 +1,40 @@
 package cn.maiaimei.example;
 
+import com.bazaarvoice.jolt.Modifier;
+import com.bazaarvoice.jolt.chainr.spec.ChainrEntry;
 import org.junit.jupiter.api.Test;
 
+/**
+ * see {@link ChainrEntry} and {@link Modifier}
+ */
 public class ModifierTest extends AbstractTest {
 
   @Test
-  void test_split() {
-    doTest("/modifier/split.json");
+  void test_isPresent() {
+    doTest("/modifier/isPresent.json");
   }
 
   @Test
-  void test_leftPad() {
-    doTest("/modifier/leftPad.json");
+  void test_isNull() {
+    doTest("/modifier/isNull.json");
+    System.out.println("--------------------");
+    doTest("/modifier/isNull2.json");
+  }
+
+  @Test
+  void test_notNull() {
+    doTest("/modifier/notNull.json");
+  }
+
+  @Test
+  void test_split_date() {
+    // "9/15/2023" -> ["9","15","2023"]
+    doTest("/modifier/split-date.json");
+  }
+
+  @Test
+  void test_format_date() {
+    // "9/2/2023" -> "09/02/2023"
+    doTest("/modifier/format-date.json");
   }
 }
--- jolt-samples/src/test/java/cn/maiaimei/example/OperationTest.java
+++ jolt-samples/src/test/java/cn/maiaimei/example/OperationTest.java
@@ -10,22 +10,40 @@ public class OperationTest extends AbstractTest {
 
   @Test
   void test_shift() {
+    // TODO: shift.json
     doTest("/operation/shift.json");
   }
 
+  /**
+   * default：功能类似 modify-default-beta ，两者有什么区别？
+   */
   @Test
   void test_default() {
     doTest("/operation/default.json");
   }
 
+  /**
+   * modify-overwrite-beta：创建缺少的键/索引，如果键/索引存在则覆盖该值。
+   */
+  @Test
+  void test_modify_overwrite_beta() {
+    doTest("/operation/modify-overwrite-beta.json");
+  }
+
+  /**
+   * modify-default-beta：创建缺少的键/索引，如果键/索引的值为null则覆盖该值。
+   */
   @Test
   void test_modify_default_beta() {
     doTest("/operation/modify-default-beta.json");
   }
 
+  /**
+   * modify-define-beta：创建缺少的键/索引
+   */
   @Test
-  void test_modify_overwrite_beta() {
-    doTest("/operation/modify-overwrite-beta.json");
+  void test_modify_define_beta() {
+    doTest("/operation/modify-define-beta.json");
   }
 
   @Test
